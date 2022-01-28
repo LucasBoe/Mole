a5910b8b1ac1b822daa63e246809dbc514729a48
LucasBoe <lucas@lumotex.de>
2022-01-28 20:25:49 +0100

    worked on level 3 and added door layer switches

--- Assets/Scripts/IndoorOutdoorSystem/DoorSwitch.cs
+++ Assets/Scripts/IndoorOutdoorSystem/DoorSwitch.cs
@@ -1,35 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorSwitch : MonoBehaviour
+public class DoorSwitch : PlayerAboveInteractable
 {
-    public bool Right;
-    // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] Layers layerLeadsTo;
+    protected override void OnPlayerEnter()
     {
-        if (collision.IsPlayer())
-            UpdateIndoorOutdoor(collision.transform.position, Right);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.IsPlayer())
-            UpdateIndoorOutdoor(collision.transform.position, !Right);
-    }
-    private void UpdateIndoorOutdoor(Vector3 position, bool leaving)
-    {
-        bool indoor = position.x < transform.position.x;
-        LayerHandler.Instance.SetIndoorOutdoor(indoor == leaving);
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        Vector2 pos = (Vector2)transform.position + (Right ? Vector2.left : Vector2.right) * 0.5f;
-        Gizmos.DrawLine(pos + Vector2.left * 0.5f, pos + Vector2.right * 0.5f);
-        Gizmos.DrawLine(pos + Vector2.up * 0.5f, pos + (Right ? Vector2.left : Vector2.right) * 0.5f);
-        Gizmos.DrawLine(pos + Vector2.down * 0.5f, pos + (Right ? Vector2.left : Vector2.right) * 0.5f);
+        LayerHandler.Instance.SetLayer(layerLeadsTo);
     }
 }
