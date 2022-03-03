a1f9ea16884bca589edf114813f0da674afc6db0
LucasBoe <lucas@lumotex.de>
2022-03-03 08:07:11 +0100

    Added Light & Dark Areas for hiding

--- Assets/Scripts/Player/Features/PlayerHidingHandler.cs
+++ Assets/Scripts/Player/Features/PlayerHidingHandler.cs
@@ -1,19 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerHidingHandler : SingletonBehaviour<PlayerHidingHandler>
 {
     public float PlayerHiddenValue => hiddenByState ? 0 : playerHiddenValue;
-    private float playerHiddenValue;
+    [SerializeField, ReadOnly] private float playerHiddenValue;
 
     private bool hiddenByState = false;
+    [SerializeField, ReadOnly] private List<PlayerHidingTrigger> playerTriggers = new List<PlayerHidingTrigger>();
 
     private void Start()
     {
         PlayerBrightnessSampler.OnSampleNewPlayerBrightness += UpdateHiddenValueFromPlayerSample;
         PlayerStateMachine.Instance.OnStateChange += OnPlayerEnterState;
+        PlayerHidingTrigger.TriggerEntered += OnTriggerEntered;
+        PlayerHidingTrigger.TriggerExited += OnTriggerExited;
+    }
+
+    private void OnTriggerExited(PlayerHidingTrigger trigger)
+    {
+        if (playerTriggers.Contains(trigger))
+            playerTriggers.Remove(trigger);
+
+        UpdateHiddenValueFromTriggers(playerTriggers);
+    }
+
+    private void OnTriggerEntered(PlayerHidingTrigger trigger)
+    {
+        if (!playerTriggers.Contains(trigger))
+            playerTriggers.Add(trigger);
+
+        UpdateHiddenValueFromTriggers(playerTriggers);
     }
 
     private void OnPlayerEnterState(PlayerStateBase state)
@@ -32,8 +52,29 @@ public class PlayerHidingHandler : SingletonBehaviour<PlayerHidingHandler>
         }
     }
 
+    private void UpdateHiddenValueFromTriggers(List<PlayerHidingTrigger> playerTriggers)
+    {
+        Debug.Log($"playerTriggers = { playerTriggers.Count }");
+
+        if (playerTriggers.Count == 0)
+            playerHiddenValue = 0.5f;
+        else
+        {
+            HiddenAmount smallest = playerTriggers.OrderBy(t => t.HiddenAmount).First().HiddenAmount;
+            playerHiddenValue = ((int)smallest) / 2f;
+            Debug.Log($"playerHiddenValue = { smallest } => { playerHiddenValue }");
+        }
+    }
+
     private void UpdateHiddenValueFromPlayerSample(float sample)
     {
-        playerHiddenValue = (sample * 3.5f) - 0.1f;
+        //playerHiddenValue = (sample * 3.5f) - 0.1f;
+    }
+
+    public enum HiddenAmount
+    {
+        Hidden,
+        Neutral,
+        Visible,
     }
 }
