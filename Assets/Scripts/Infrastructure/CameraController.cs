5fed1444db2a9ec50d33cdc9fcb34de5bef05e7d
LucasBoe <lucas@lumotex.de>
2022-03-04 12:47:27 +0100

    Added Render Mode Switch

--- Assets/Scripts/Infrastructure/CameraController.cs
+++ Assets/Scripts/Infrastructure/CameraController.cs
@@ -8,17 +8,24 @@ public class CameraController : SingletonBehaviour<CameraController>
 {
     [SerializeField] private RenderTexture renderTexture;
     [SerializeField] private Transform[] textureDisplayTransforms;
-    [SerializeField] private new Camera camera;
+    [SerializeField] private Camera cameraPixel;
+    [SerializeField] private Camera cameraRaw;
     [SerializeField] private bool smooth;
 
     CinemachineVirtualCamera virtualCamera;
     CinemachineFramingTransposer transposer;
+
+    List<RenderModes> allRenderModes = new List<RenderModes>();
+    int renderModeIndex = 1;
 
     private void OnEnable()
     {
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
         transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
         PlayerController.OnPlayerSpawned += ConnectPlayerTransformToVirtualCamera;
+
+        foreach (RenderModes mode in Enum.GetValues(typeof(RenderModes)))
+            allRenderModes.Add(mode);
     }
 
 
@@ -31,19 +38,43 @@ public class CameraController : SingletonBehaviour<CameraController>
     public static Vector2 ScreenToWorldPoint(Vector2 vector2)
     {
         Vector2 remap = new Vector2((vector2.x / Screen.width) * Instance.renderTexture.width, (vector2.y / Screen.height) * Instance.renderTexture.height);
-        return Instance.camera.ScreenToWorldPoint(remap);
+        return Instance.cameraPixel.ScreenToWorldPoint(remap);
     }
 
     internal static Vector3 WorldToScreenPoint(Vector3 worldPos)
     {
-        Vector2 raw = Instance.camera.WorldToScreenPoint(worldPos);
+        Vector2 raw = Instance.cameraPixel.WorldToScreenPoint(worldPos);
         return new Vector2((raw.x / Instance.renderTexture.width) * Screen.width, (raw.y / Instance.renderTexture.height) * Screen.height);
     }
 
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.F3))
+        {
+            renderModeIndex--;
+            if (renderModeIndex < 0)
+                renderModeIndex = allRenderModes.Count - 1;
+
+            UpdateRenderMode(allRenderModes[renderModeIndex]);
+        }
 
         transposer.m_TrackedObjectOffset = PlayerInputHandler.PlayerInput.VirtualCursorToScreenCenter * new Vector2(4, 3);
+    }
+
+    private void UpdateRenderMode(RenderModes renderMode)
+    {
+        switch (renderMode)
+        {
+            case RenderModes.RAW:
+                cameraRaw.gameObject.SetActive(true);
+                cameraPixel.gameObject.SetActive(false);
+                break;
+
+            case RenderModes.PIXELOLD:
+                cameraRaw.gameObject.SetActive(false);
+                cameraPixel.gameObject.SetActive(true);
+                break;
+        }
     }
 
     private void LateUpdate()
@@ -53,7 +84,7 @@ public class CameraController : SingletonBehaviour<CameraController>
 
     private void SmoothCamera()
     {
-        Vector2 camPosRaw = camera.transform.position;
+        Vector2 camPosRaw = cameraPixel.transform.position;
         Vector2 camPosRounded = new Vector2(RoundTo8PixelPerUnit(camPosRaw.x), RoundTo8PixelPerUnit(camPosRaw.y));
         Vector3 rest = (Vector3)(camPosRounded - camPosRaw);
 
@@ -67,5 +98,12 @@ public class CameraController : SingletonBehaviour<CameraController>
     private float RoundTo8PixelPerUnit(float raw)
     {
         return Mathf.Round(raw * 8f) / 8f;
+    }
+
+    public enum RenderModes
+    {
+        RAW,
+        PIXELOLD,
+        PIXELNEW,
     }
 }
