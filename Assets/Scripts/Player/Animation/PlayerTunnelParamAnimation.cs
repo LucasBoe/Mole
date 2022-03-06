046f2d208c90e8bb91aa0ced7615a589e134837a
LucasBoe <lucas@lumotex.de>
2022-03-06 22:35:02 +0100

    Added logic for implementation of new tunnel animation

---
+++ Assets/Scripts/Player/Animation/PlayerTunnelParamAnimation.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerTunnelParamAnimation : ParameterBasedAnimation<TunnelState>
+{
+    [SerializeField] Sprite[] vertical, horizontal;
+
+    private Sprite sprite;
+
+    public override void Init(PlayerStateMachine playerStateMachine)
+    {
+        base.Init(playerStateMachine);
+    }
+
+    public override Sprite Update()
+    {
+        if (sprite == null)
+            sprite = horizontal[0];
+
+        if (State.MoveDir != Vector2Int.zero)
+        {
+            sprite = GetSpriteForDir(State.MoveDir);
+        }
+
+        return sprite;
+    }
+
+    private Sprite GetSpriteForDir(Vector2Int moveDir)
+    {
+        Sprite[] sprites = null;
+
+        if (Mathf.Abs(moveDir.x) > Mathf.Abs(moveDir.y))
+            sprites = vertical;
+        else
+            sprites = horizontal;
+
+        return sprites[(int)(Mathf.Floor(Time.time * (sprites.Length + 0.5f)) % sprites.Length)];
+    }
+}
+
--- Assets/Scripts/Player/States/Other/TunnelState.cs
+++ Assets/Scripts/Player/States/Other/TunnelState.cs
@@ -6,6 +6,7 @@ using System;
 
 public class TunnelState : PlayerStateBase
 {
+    public Vector2Int MoveDir; 
     public TunnelState(Transform switchTransform)
     {
         context.Rigidbody.MovePosition(switchTransform.position);
@@ -13,6 +14,14 @@ public class TunnelState : PlayerStateBase
 
     public override void Update()
     {
-        context.Rigidbody.velocity = context.Input.Axis * context.Values.XVelocity.NotSprintValue;
+        Vector2 axis = context.Input.Axis;
+        if (axis == Vector2.zero)
+            MoveDir = Vector2Int.zero;
+        else if (Mathf.Abs(axis.x) > Mathf.Abs(axis.y))
+            MoveDir = axis.x >= 0 ? Vector2Int.right : Vector2Int.left;
+        else
+            MoveDir = axis.y >= 0 ? Vector2Int.up : Vector2Int.down;
+
+        context.Rigidbody.velocity = axis * context.Values.XVelocity.NotSprintValue;
     }
 }
