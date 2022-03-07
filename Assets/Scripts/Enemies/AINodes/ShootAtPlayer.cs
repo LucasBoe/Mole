e690a6d8e95be2053eaa4951ed27052cfdb609cb
LucasBoe <lucas@lumotex.de>
2022-03-07 19:48:34 +0100

    Enemy does not get stuck in shooting

--- Assets/Scripts/Enemies/AINodes/ShootAtPlayer.cs
+++ Assets/Scripts/Enemies/AINodes/ShootAtPlayer.cs
@@ -17,7 +17,9 @@ public class ShootAtPlayer : ActionNode
             Vector2 playerPos = context.PlayerPos;
             context.memory.Forward = playerPos.x < context.transform.position.x ? Direction2D.Left : Direction2D.Right;
             context.shootModule.Shoot(playerPos, shootingVelocity);
+            return State.Success;
         }
-        return State.Success;
+
+        return State.Failure;
     }
 }
