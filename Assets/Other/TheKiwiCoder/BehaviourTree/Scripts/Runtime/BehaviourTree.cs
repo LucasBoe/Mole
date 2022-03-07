ff1b2d8e9e157c899760ad5c2cc70c39ecedf1d0
LucasBoe <lucas@lumotex.de>
2022-03-07 16:39:05 +0100

    Added subtree logic and example with patroling enemy

--- Assets/Other/TheKiwiCoder/BehaviourTree/Scripts/Runtime/BehaviourTree.cs
+++ Assets/Other/TheKiwiCoder/BehaviourTree/Scripts/Runtime/BehaviourTree.cs
@@ -6,62 +6,84 @@ using UnityEditor;
 #endif
 
 
-namespace TheKiwiCoder {
+namespace TheKiwiCoder
+{
     [CreateAssetMenu()]
-    public class BehaviourTree : ScriptableObject {
+    public class BehaviourTree : ScriptableObject
+    {
         public Node rootNode;
+        public RootNode RootNode => rootNode as RootNode;
         public Node.State treeState = Node.State.Running;
         public List<Node> nodes = new List<Node>();
         public Blackboard blackboard = new Blackboard();
 
-        public Node.State Update() {
-            if (rootNode.state == Node.State.Running) {
+        public Node.State Update()
+        {
+            if (rootNode.state == Node.State.Running)
+            {
                 treeState = rootNode.Update();
             }
             return treeState;
         }
 
-        public static List<Node> GetChildren(Node parent) {
+        public static List<Node> GetChildren(Node parent)
+        {
             List<Node> children = new List<Node>();
 
-            if (parent is DecoratorNode decorator && decorator.child != null) {
+            if (parent is DecoratorNode decorator && decorator.child != null)
+            {
                 children.Add(decorator.child);
             }
 
-            if (parent is RootNode rootNode && rootNode.child != null) {
+            if (parent is RootNode rootNode && rootNode.child != null)
+            {
                 children.Add(rootNode.child);
             }
 
-            if (parent is CompositeNode composite) {
+            if (parent is CompositeNode composite)
+            {
                 return composite.children;
+            }
+
+            if (parent is SubTree subTree)
+            {
+                children.Add(subTree.Tree.rootNode);
             }
 
             return children;
         }
 
-        public static void Traverse(Node node, System.Action<Node> visiter) {
-            if (node) {
+        public static void Traverse(Node node, System.Action<Node> visiter)
+        {
+            if (node)
+            {
                 visiter.Invoke(node);
                 var children = GetChildren(node);
                 children.ForEach((n) => Traverse(n, visiter));
             }
         }
 
-        public BehaviourTree Clone() {
+        public BehaviourTree Clone()
+        {
             BehaviourTree tree = Instantiate(this);
             tree.rootNode = tree.rootNode.Clone();
             tree.nodes = new List<Node>();
-            Traverse(tree.rootNode, (n) => {
+            Traverse(tree.rootNode, (n) =>
+            {
                 tree.nodes.Add(n);
             });
 
             return tree;
         }
 
-        public void Bind(Context context) {
-            Traverse(rootNode, node => {
-                node.context = context;
-                node.blackboard = blackboard;
+        public void Bind(Context context, Blackboard customBlackboard = null)
+        {
+            Traverse(rootNode, node =>
+            {
+                if (customBlackboard != null)
+                    node.Bind(context, customBlackboard);
+                else
+                    node.Bind(context, blackboard);
             });
         }
 
@@ -69,7 +91,8 @@ namespace TheKiwiCoder {
         #region Editor Compatibility
 #if UNITY_EDITOR
 
-        public Node CreateNode(System.Type type) {
+        public Node CreateNode(System.Type type)
+        {
             Node node = ScriptableObject.CreateInstance(type) as Node;
             node.name = type.Name;
             node.guid = GUID.Generate().ToString();
@@ -77,7 +100,8 @@ namespace TheKiwiCoder {
             Undo.RecordObject(this, "Behaviour Tree (CreateNode)");
             nodes.Add(node);
 
-            if (!Application.isPlaying) {
+            if (!Application.isPlaying)
+            {
                 AssetDatabase.AddObjectToAsset(node, this);
             }
 
@@ -87,7 +111,8 @@ namespace TheKiwiCoder {
             return node;
         }
 
-        public void DeleteNode(Node node) {
+        public void DeleteNode(Node node)
+        {
             Undo.RecordObject(this, "Behaviour Tree (DeleteNode)");
             nodes.Remove(node);
 
@@ -97,40 +122,48 @@ namespace TheKiwiCoder {
             AssetDatabase.SaveAssets();
         }
 
-        public void AddChild(Node parent, Node child) {
-            if (parent is DecoratorNode decorator) {
+        public void AddChild(Node parent, Node child)
+        {
+            if (parent is DecoratorNode decorator)
+            {
                 Undo.RecordObject(decorator, "Behaviour Tree (AddChild)");
                 decorator.child = child;
                 EditorUtility.SetDirty(decorator);
             }
 
-            if (parent is RootNode rootNode) {
+            if (parent is RootNode rootNode)
+            {
                 Undo.RecordObject(rootNode, "Behaviour Tree (AddChild)");
                 rootNode.child = child;
                 EditorUtility.SetDirty(rootNode);
             }
 
-            if (parent is CompositeNode composite) {
+            if (parent is CompositeNode composite)
+            {
                 Undo.RecordObject(composite, "Behaviour Tree (AddChild)");
                 composite.children.Add(child);
                 EditorUtility.SetDirty(composite);
             }
         }
 
-        public void RemoveChild(Node parent, Node child) {
-            if (parent is DecoratorNode decorator) {
+        public void RemoveChild(Node parent, Node child)
+        {
+            if (parent is DecoratorNode decorator)
+            {
                 Undo.RecordObject(decorator, "Behaviour Tree (RemoveChild)");
                 decorator.child = null;
                 EditorUtility.SetDirty(decorator);
             }
 
-            if (parent is RootNode rootNode) {
+            if (parent is RootNode rootNode)
+            {
                 Undo.RecordObject(rootNode, "Behaviour Tree (RemoveChild)");
                 rootNode.child = null;
                 EditorUtility.SetDirty(rootNode);
             }
 
-            if (parent is CompositeNode composite) {
+            if (parent is CompositeNode composite)
+            {
                 Undo.RecordObject(composite, "Behaviour Tree (RemoveChild)");
                 composite.children.Remove(child);
                 EditorUtility.SetDirty(composite);
---
+++ Assets/Scripts/Enemies/AINodes/SubTree.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class SubTree : ActionNode
+{
+    public BehaviourTree Tree;
+    public BehaviourTree TreeInstance;
+    protected override void OnStart()  { }
+
+    protected override void OnStop() { }
+
+    protected override State OnUpdate()
+    {
+        return TreeInstance.rootNode.Update();
+    }
+
+    public override void Bind(Context context, Blackboard blackboard)
+    {
+        TreeInstance = Tree.Clone();
+        base.Bind(context, blackboard);
+        TreeInstance.Bind(context, blackboard);
+    }
+}
