7186669baa25dd7436cfc23007e39bf432278b46
LucasBoe <lucas@lumotex.de>
2022-04-26 14:47:36 +0200

    Added Key for unlocking doors

--- Assets/Scripts/IndoorOutdoorSystem/LayerSwitchLock.cs
+++ Assets/Scripts/IndoorOutdoorSystem/LayerSwitchLock.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 //TODO: Add item based unlocking for switches
 [RequireComponent(typeof(LayerSwitch))]
@@ -9,6 +10,9 @@ public class LayerSwitchLock : MonoBehaviour
 {
     public bool IsLocked;
     private LayerSwitch layerSwitch;
+
+    [SerializeField] bool HasKeyItem;
+    [SerializeField, ShowIf("HasKeyItem")] PlayerItem keyItem;
     private void OnValidate()
     {
         LayerSwitch layerSwitch = GetComponent<LayerSwitch>();
@@ -22,8 +26,23 @@ public class LayerSwitchLock : MonoBehaviour
 
     internal bool TryUnlock()
     {
-
         if (!IsLocked) return true;
+
+        if (HasKeyItem)
+        {
+            if (PlayerItemHolder.Instance.GetAmount(keyItem) > 0)
+            {
+                PlayerItemHolder.Instance.RemoveItem(keyItem);
+                IsLocked = false;
+                WorldTextSpawner.Spawn("used " + keyItem.name, transform.position);
+                return true;
+
+            } else
+            {
+                WorldTextSpawner.Spawn("0 / 1 " + keyItem.name, transform.position);
+                return false;
+            }
+        }
 
         WorldTextSpawner.Spawn("This door is locked...", transform.position);
 
